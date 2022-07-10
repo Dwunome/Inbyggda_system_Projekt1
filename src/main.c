2861c908811e27eee61e0d7ad1c932a9f52fadf9
Markus <99958340+Dwunome@users.noreply.github.com>
2022-07-10 18:26:57 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,19 @@
+// Inkluderingsdirektiv:
+#include "header.h"
 
+/******************************************************************************
+* Funktionen main utgör programmets start- och slutpunkt. Programmets globala
+* variabler initieras via anrop av funktionen setup. En while-sats används för
+* att hålla igång programmet så länge matningsspänning tillförs.
+*
+* EDIT: Bytte ut goto-satsen mot en while-sats.
+******************************************************************************/
+int main(void)
+{	
+	setup();	
+    while(true)
+	{
+			
+	}
+	return 0;
+}
