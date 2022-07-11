9598595943018441283ad0487739cfde25b76bcb
Markus <99958340+Dwunome@users.noreply.github.com>
2022-07-11 17:02:43 +0200

    Update ADC.h

--- src/ADC.h
+++ src/ADC.h
@@ -7,42 +7,44 @@
 #include "Serial.h"
 
 /******************************************************************************
-* Formler för beräkning av temperatur:
+* Formler fÃ¶r berÃ¤kning av temperatur:
 *
-* Analog inspänning Uin:
+* Analog inspÃ¤nning Uin:
 * Uin = Vcc * ADC_result / ADC_max,
-* där ADC_result är resultatet från senaste AD-omvandling (0 - 1023).
+* dÃ¤r ADC_result Ã¤r resultatet frÃ¥n senaste AD-omvandling (0 - 1023).
 *
 * Temperatur temp i grader Celcius:
 * temp = 100 * Uin - 50,
-* där Uin är den beräknade analoga inspänningen (0 - 5 V).
+* dÃ¤r Uin Ã¤r den berÃ¤knade analoga inspÃ¤nningen (0 - 5 V).
 ******************************************************************************/
-#define VCC 5.0f // Matningsspänning 5 V.
-#define ADC_MAX 1023.0 // Maxvärde vid AD-omvandling. Ändrade till 1023.0
+
+#define VCC 5.0f // MatningsspÃ¤nning 5 V.
+#define ADC_MAX 1023.0 // MaxvÃ¤rde vid AD-omvandling. Ã„ndrade till 1023.0
 
 /******************************************************************************
-* För att välja intern matningsspänning för AD-omvandlaren, så ettställs biten
-* REFS0 i register ADMUX (ADC Multiplexer Select Register). Som default läses
-* analog PIN A0 av. För att starta AD-omvandlingen så ettställs samtliga
-* bitar i registret ADCSRA (ADC Control and Status Register A). För att 
-* kontrollera så att en given AD-omvandling är slutförd innan resultatet 
-* hämtas, så sker avläsning av biten ADIF (ADC Interrupt Flag), så blir 
-* ettställt när AD-omvandlingen är färdig. Så länge denna bit är nollställd,
-* så väntar vi in att AD-omvandlingen är färdig via en while-sats. Biten
-* ADCSRA återställs inför nästa AD-omvandling genom att denna nollställs.
+* FÃ¶r att vÃ¤lja intern matningsspÃ¤nning fÃ¶r AD-omvandlaren, sÃ¥ ettstÃ¤lls biten
+* REFS0 i register ADMUX (ADC Multiplexer Select Register). Som default lÃ¤ses
+* analog PIN A0 av. FÃ¶r att starta AD-omvandlingen sÃ¥ ettstÃ¤lls samtliga
+* bitar i registret ADCSRA (ADC Control and Status Register A). FÃ¶r att 
+* kontrollera sÃ¥ att en given AD-omvandling Ã¤r slutfÃ¶rd innan resultatet 
+* hÃ¤mtas, sÃ¥ sker avlÃ¤sning av biten ADIF (ADC Interrupt Flag), sÃ¥ blir 
+* ettstÃ¤llt nÃ¤r AD-omvandlingen Ã¤r fÃ¤rdig. SÃ¥ lÃ¤nge denna bit Ã¤r nollstÃ¤lld,
+* sÃ¥ vÃ¤ntar vi in att AD-omvandlingen Ã¤r fÃ¤rdig via en while-sats. Biten
+* ADCSRA Ã¥terstÃ¤lls infÃ¶r nÃ¤sta AD-omvandling genom att denna nollstÃ¤lls.
 ******************************************************************************/
+
 #define SELECT_ANALOG_CHANNEL ADMUX = (1 << REFS0)
 #define START_AD_CONVERSION ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2)
 #define WAIT_FOR_AD_CONVERSION_COMPLETE while ((ADCSRA & (1 << ADIF)) == 0)
 #define RESET_ADC_INTERRUPT_FLAG ADCSRA = (1 << ADIF)
 
 /******************************************************************************
-* Strukten TempSensor används för implementering av en temperatursensor
+* Strukten TempSensor anvÃ¤nds fÃ¶r implementering av en temperatursensor
 * ansluten till en given analog PIN A0 - A5.
 ******************************************************************************/
 struct TempSensor 
 {
-	uint8_t PIN; // PIN-nummer för avläsning.
+	uint8_t PIN; // PIN-nummer fÃ¶r avlÃ¤sning.
 };
 
 // Funktionsdeklarationer:
