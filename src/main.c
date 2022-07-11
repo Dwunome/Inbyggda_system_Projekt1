9ed19a20911b6d09fe93887bf0129034d7687436
Markus <99958340+Dwunome@users.noreply.github.com>
2022-07-11 17:17:04 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 * Funktionen main utgör programmets start- och slutpunkt. Programmets globala
 * variabler initieras via anrop av funktionen setup. En while-sats används för
 * att hålla igång programmet så länge matningsspänning tillförs.
-*
-* EDIT: Bytte ut goto-satsen mot en while-sats.
 ******************************************************************************/
 int main(void)
 {	
